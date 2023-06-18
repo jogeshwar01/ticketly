28809473432d5bbee76d9fc1d13a7121a6ce1834
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2023-06-18 13:42:17 +0530

    setup mongo - mongoose

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import 'express-async-errors';
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -18,18 +19,26 @@ app.use(signoutRouter);
 app.use(signupRouter);
 
 // app.all - for any kind of method and * is for any kind of route
+// generally next is needed for async error handler - this is default behaviour -  next( new NotFoundError());
+// so instead to use throw with async code - we would download a new package express-async-errors
 app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
 
-// app.all("*",async (req,res,next) => {
-//   next( new NotFoundError());
-// });
-// next is needed for async error handler - this is default behaviour
-// so instead to use throw with async code - we would download a new package express-async-errors
-
 app.use(errorHandler);
+
+const start = async () => {
+  try {
+    // need to use cluster ip service to connect to mongo instance
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to MongoDb');
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!");
 });
+
+start();
