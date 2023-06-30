59ce300c5963e6b686b1672b01eb5ee54181c45d
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2023-06-30 15:42:50 +0530

    configure k8s for tickets and add  mongo connection uri

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,9 +8,13 @@ const start = async () => {
     throw new Error('JWT_KEY must be defined');
   }
 
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
+
   try {
     // need to use cluster ip service to connect to mongo instance
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDb');
   } catch (err) {
     console.error(err);
