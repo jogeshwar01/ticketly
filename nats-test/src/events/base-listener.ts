7b6f66583372de512f7fd22212e2012e5ef93cce
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2023-07-08 15:31:31 +0530

    add base class for listener - code reuse

---
+++ nats-test/src/events/base-listener.ts
@@ -0,0 +1,46 @@
+import { Message, Stan } from 'node-nats-streaming';
+
+export abstract class Listener {
+  abstract subject: string; //name of channel this listener would listen to
+  abstract queueGroupName: string; //the one the listener will join
+  abstract onMessage(data: any, msg: Message): void; //fn to run when msg received
+  private client: Stan; //pre initialised stan client
+  protected ackWait = 5 * 1000; //no. of seconds this listener has to ack a msg
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable()             // get all events emitted ever - useful when new service created
+      .setManualAckMode(true)               // only when manually acknowledged do we consider an event processed
+      .setAckWait(this.ackWait)             //if we dont set anything it is 30 seconds
+      .setDurableName(this.queueGroupName); // durable subscription - to get only the un-processed events 
+                                            // as it contains details of all events and whether they are processed or not
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,    // helps when we even temporarily disconnect services in a group, the durable sub won't be dumped by nats
+      this.subscriptionOptions()
+    );
+
+    // what to do when an msg received over the channel
+    subscription.on('message', (msg: Message) => {
+      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
+
+      const parsedData = this.parseMessage(msg);
+      this.onMessage(parsedData, msg);
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData(); // data can be string/buffer -can get these getData and other functions from type def file or docs
+    return typeof data === 'string'
+      ? JSON.parse(data)
+      : JSON.parse(data.toString('utf8')); // for type buffer - not expected though
+  }
+}
--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
 import nats, { Message } from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
+import { TicketCreatedListener } from './events/ticket-created-listener';
 
 console.clear();
 
@@ -16,30 +17,7 @@ stan.on('connect', () => {
     process.exit();
   });
 
-  const options = stan
-  .subscriptionOptions()
-  .setManualAckMode(true)                 // only when manually acknowledged do we consider an event processed
-  .setDeliverAllAvailable()               // get all events emitted ever - useful when new service created
-  .setDurableName('accounting-service');  // durable subscription - to get only the un-processed events 
-                                          // as it contains details of all events and whether they are processed or not
-  
-  // subscribing to channel ticket:created
-  const subscription = stan.subscribe(
-    'ticket:created',
-    'queue-group-name', // helps when we even temporarily disconnect services in a group, the durable sub won't be dumped by nats
-    options
-  );
-
-  // what to do when an msg received over the channel
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData();     // can get these getData and other functions from type def file or docs
-
-    if (typeof data === 'string') {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-    }
-
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 // Graceful client shutdown - shutdown old when restarted without waiting for 30 seconds
