d2a6078e557985e1aa48b2e6bff7b37fedfb1df2
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2023-07-12 21:43:00 +0530

    add nats env variables (client id based on name)

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,16 +6,27 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
-
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined');
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be defined");
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL must be defined");
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be defined");
   }
 
   try {
     // here ticketly is cluster id specified in nats-depl and nats-src is the service related to nats-depl
     // alsdj - random client id - should be different for all
-    await natsWrapper.connect('ticketly', 'alsdkj', 'http://nats-srv:4222');
-    
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );    
     // called when stan.close() called
     natsWrapper.client.on('close', () => {
       console.log('NATS connection closed!');
