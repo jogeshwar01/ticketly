843fe0a4978438c5d15c80bed6e09e7bf4116269
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2023-08-06 15:40:38 +0530

    reserve ticket with order id - order created listener

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -12,6 +12,7 @@ interface TicketDoc extends mongoose.Document {
   price: number;
   userId: string;
   version: number;
+  orderId?: string;  // ? -so that it can be undefined/null when ticket is created initially
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -31,6 +32,9 @@ const ticketSchema = new mongoose.Schema(
     userId: {
       type: String,
       required: true,
+    },
+    orderId: {
+      type: String,
     },
   },
   {
