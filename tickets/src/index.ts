2ba5219414179c7b96b8b201d6285203dd95f916
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2023-08-12 14:15:19 +0530

    add order cancelled listener and reject edits for reserved tickets

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
+import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -37,6 +39,9 @@ const start = async () => {
     // Graceful client shutdown - shutdown old when restarted without waiting for 30 seconds
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
+
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDb');
