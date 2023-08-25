5e141ce4a67064696c14032dc2376ac52f45e9d8
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2023-08-25 22:35:10 +0530

    add event complete event and tests

---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,39 @@
+import {
+  Listener,
+  Subjects,
+  ExpirationCompleteEvent,
+  OrderStatus,
+} from '@js-ticketly/common';
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+import { Order } from '../../models/order';
+import { OrderCancelledPublisher } from '../publishers/order-cancelled-publisher';
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+  queueGroupName = queueGroupName;
+  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+
+  async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+    const order = await Order.findById(data.orderId).populate('ticket');
+
+    if (!order) {
+      throw new Error('Order not found');
+    }
+
+    order.set({
+      status: OrderStatus.Cancelled,
+    });
+    // we don't set ticket to null here - so that we can see the tickets in orders that were cancelled
+    // we can do this as our model keeps a ticket reserved based on status only
+    await order.save();
+    await new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
+
+    msg.ack();
+  }
+}
