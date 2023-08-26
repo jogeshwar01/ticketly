3ea6218eb9d555cc9ce2c1df3d5699ca6c061f2d
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2023-08-26 17:42:20 +0530

    setup payments service

---
+++ payments/src/app.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import 'express-async-errors';
+import { json } from "body-parser";
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from "@js-ticketly/common";
+
+const app = express();
+app.set('trust proxy', true);
+
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+  })
+);
+
+app.use(currentUser); //make sure to add it after cookie session so that req.session is set
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ payments/src/test/setup.ts
@@ -0,0 +1,70 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../app';
+import jwt from 'jsonwebtoken';
+
+declare global {
+  var signin: () => string[];
+}
+// doing global just for ease of use - can create a auth Helper separately and import wherever needed also
+// this wont be available in our normal app code so only for testing environment
+
+jest.mock("../nats-wrapper");
+// this is going to create a mock of the nats-wrapper class and it would look into the __mocks__ folder for the fake one
+
+let mongo: any;
+
+//this will run before all our tests start to run
+beforeAll(async () => {
+  process.env.JWT_KEY = 'asdfasdf';
+  // we need to write this as our env variables should exists for tests as well
+
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri, {});
+});
+
+//this will run before each of our test - reset data in mongo
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  if (mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
+});
+
+// to get cookie - fake signin for tests by creating dummy user and generating jwt
+global.signin = () => {
+  // Build a JWT payload. { id, email}
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),  //random id everytime
+    email: "test@test.com",
+  };
+
+  // Create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Build session Object. { jwt: MY_JWT}
+  const session = { jwt: token };
+
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Take JSON and encode it as base64 - this is just needed - learn syntax
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  // return a string thats the cookie with the encoded data
+  return [`session=${base64}`]; // array as supertest likes it
+};
