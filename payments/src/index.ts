f3e1be476385d1c921228e70a335d45a87f0a4a8
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2023-08-27 13:36:27 +0530

    create model and listeners in payments service

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
+import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -26,7 +28,7 @@ const start = async () => {
       process.env.NATS_CLUSTER_ID,
       process.env.NATS_CLIENT_ID,
       process.env.NATS_URL
-    );    
+    );
     // called when stan.close() called
     natsWrapper.client.on('close', () => {
       console.log('NATS connection closed!');
@@ -37,6 +39,9 @@ const start = async () => {
     // Graceful client shutdown - shutdown old when restarted without waiting for 30 seconds
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
+
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDb');
