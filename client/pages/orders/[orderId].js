c0768a8d91bc2407b2f3d2e9d1dbe381b2ba2194
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2023-09-03 16:40:28 +0530

    add ticket and order show pages

---
+++ client/pages/orders/[orderId].js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+
+const OrderShow = ({ order, currentUser }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+
+    findTimeLeft(); //to display time as the page loads
+    const timerId = setInterval(findTimeLeft, 1000); //update timer after each second
+
+    // when fn returned from useEffect - this is called when either component re-rendered (if dependency listed ) 
+    // or if you are trying to navigate away from the component
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [order]);
+
+  if (timeLeft < 0) {
+    return <div>Order Expired</div>;
+  }
+
+  return (
+    <div>
+      Time left to pay: {timeLeft} seconds
+    </div>
+  );
+};
+
+OrderShow.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;
+  const { data } = await client.get(`/api/orders/${orderId}`);
+
+  return { order: data };
+};
+
+export default OrderShow;
