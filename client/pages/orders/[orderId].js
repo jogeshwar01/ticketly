a79a5663e2398b2fa0054d9d4458d5899c4360c8
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2023-09-03 16:51:37 +0530

    add stripe checkout and orders page

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,7 +1,18 @@
 import { useEffect, useState } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
 
 const OrderShow = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState(0);
+  const { doRequest, errors } = useRequest({
+    url: '/api/payments',
+    method: 'post',
+    body: {
+      orderId: order.id,
+    },
+    onSuccess: () => Router.push('/orders'),
+  });
 
   useEffect(() => {
     const findTimeLeft = () => {
@@ -26,6 +37,12 @@ const OrderShow = ({ order, currentUser }) => {
   return (
     <div>
       Time left to pay: {timeLeft} seconds
+      <StripeCheckout
+        token={({ id }) => doRequest({ token: id })}
+        stripeKey="pk_test_51JzfKQSAJ3mr6KzPp0ThmkfhgCtYBhJYZfyOHW49xhHEy9QRF2GxaEETpkCKx6YWTpqZZDTPKMOFMBm36FXCVyxF00H5GwRxdp"
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+      />
     </div>
   );
 };
